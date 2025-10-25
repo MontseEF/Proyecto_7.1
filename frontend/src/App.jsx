a3076a88d0e176cb7f8163091abe6f78f8c95f55
MontseEF <monfloresespinoza29@gmail.com>
2025-10-24 22:48:51 -0300

    front_connection

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-function App() {
+import Products from './pages/Products'
+
+export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        ¡Hola Montse con Vite + React + SWC + Tailwind!
-      </h1>
-    </div>
+    <main className="min-h-screen">
+      <Products />
+    </main>
   )
 }
-
-export default App
